90c618b0ecb216a56965704095848b04fcb67efb
amitabh <buddhabacchan@gmail.com>
2021-01-21 21:02:00 -0800

    updating missing_albums_in_google

---
+++ tasks/missing_albums_in_google.py
@@ -0,0 +1,31 @@
+from gphoto.google_images import GoogleImages
+from gphoto import google_images
+from gphoto.pics_folder import PicsFolder
+from gphoto.google_library import GoogleLibrary
+
+
+class MissingAlbumsInGoogle(object):
+    """
+    The class assumes that the Google library cache is loaded
+    Run the following to load the cache if not already done:
+
+    import gphoto
+    from gphoto.google_library import GoogleLibrary
+    gphoto.init()
+    GoogleLibrary.load_library()
+    """
+
+    @staticmethod
+    def find(pics_folder):
+        """
+        Given a local pics folder
+        """
+        pics_folder_cache = PicsFolder.cache()
+        google_image_cache = GoogleImages.cache()
+
+        if not pics_folder_cache or not google_image_cache:
+            GoogleLibrary.load_library()
+            pics_folder_cache = PicsFolder.cache()
+            google_image_cache = GoogleImages.cache()
+
+        
