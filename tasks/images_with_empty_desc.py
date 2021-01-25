973ed034597e03fc720e1eb0f821aec0bc957c5d
AmitabhArora <amitabh.arora@chevron.com>
2021-01-25 15:13:20 -0800

    created shell images_with_empty_desc and updated refernces

--- tasks/images_with_empty_desc.py
+++ tasks/images_with_empty_desc.py
@@ -3,4 +3,6 @@ import context; context.set_context()
 class ImagesWithEmptyDesc(object):
 
     def find():
-      
+      """
+      Reference: https://akrabat.com/setting-title-and-caption-with-exiftool/
+      """
