9051666dc28dd8a63fd8856403da4bf8e98010f5
amitabh <buddhabacchan@gmail.com>
2021-01-26 21:17:56 -0800

    implementing image_dateshot_and_name_mismatched

--- tasks/image_dateshot_and_name_mismatched.py
+++ tasks/image_dateshot_and_name_mismatched.py
@@ -13,7 +13,7 @@ from gphoto.local_library import LocalLibrary
 _IMAGE_PATTERN = "20201104_083022"
 _IMAGE_PATTERN_LEN = len(_IMAGE_PATTERN)
 
-def main():
+def main_with_exiftool(et):
     """
     Images should follow the format:
     YYYYMMMDD_HHmmSS....
@@ -31,61 +31,72 @@ def main():
     images = cache['images']
     albums = cache['albums']
 
+    for image in images:
+        image_name = image['name']
+        image_path = image['path']
 
-    with exiftool.ExifTool() as et:
-        for image in images:
-            image_name = image['name']
-            image_path = image['path']
+        if not os.path.exists(image_path):
+            continue
 
-            # If image does not follow correct pattern
-            # Then add it to album list
-            mismatched = False
-            mismatch_reason = None
-            if len(image_name) < _IMAGE_PATTERN_LEN:
+        # If image does not follow correct pattern
+        # Then add it to album list
+        mismatched = False
+        mismatch_reason = None
+        if len(image_name) < _IMAGE_PATTERN_LEN:
+            mismatched = True
+            mismatch_reason = "file_name_fmt"
+
+        # if image date shot does not match images name
+        # then add it to the mismatched list.  For PNG use PNG:CreationTime
+        tag = None
+        if not mismatched:
+            tag = et.get_tag("Exif:DateTimeOriginal", image_path)
+            if tag is None or len(tag) <= 0:
                 mismatched = True
-                mismatch_reason = "file_name_fmt"
+                mismatch_reason = "missing date shot '{tag}'"
 
-            # if image date shot does not match images name
-            # then add it to the mismatched list
-            tag = None
-            if not mismatched:
-                tag = et.get_tag("Exif:DateTimeOriginal", image_path)
-                if tag is None or len(tag) <= 0:
-                    mismatched = True
-                    mismatch_reason = "missing date shot '{tag}'"
+        tagsplit = None
+        if not mismatched:
+            tagsplit = tag.split(' ')
+            if len(tagsplit) < 2:
+                mismatched = True
+                mismatch_reason = "bad date shot '{tag}'"
 
-            tagsplit = None
-            if not mismatched:
-                tagsplit = tag.split(' ')
-                if len(tagsplit) < 2:
-                    mismatched = True
-                    mismatch_reason = "bad date shot '{tag}'"
+        filedatetime = None
+        if not mismatched:
+            filedatetime = image_name.split('_')
+            if len(filedatetime) < 2:
+                mismatch_reason = "Filename FMT"
+                mismatched = True
 
-            if not mismatched:
-                filedatetime = image_name.split('_')
-                file_date = filedatetime[0]
-                file_time = filedatetime[1]
+        if not mismatched:
+            file_date = filedatetime[0]
+            file_time = filedatetime[1]
+            tag_date = ''.join(tagsplit[0].split(':'))
+            tag_time = ''.join(tagsplit[1].split(':'))
 
-                tag_date = ''.join(tagsplit[0].split(':'))
-                tag_time = ''.join(tagsplit[1].split(':'))
+            if tag_date != file_date or tag_time != file_time:
+                mismatched = True
+                mismatch_reason = "mismatched tag '{tag}'"
 
-                if tag_date != file_date or tag_time != file_time:
-                    mismatched = True
-                    mismatch_reason = "mismatched tag '{tag}'"
-
-            if mismatched:
-                parent_index = image['parent']
-                album = albums[parent_index]
-                album_path = album['path']
-                album_image_list = None
-                if album_path not in album_result:
-                    album_image_list = [image_path]
-                    album_result[album_path] = album_image_list
-                else:
-                    album_image_list = album_result[album_path]
-                    album_image_list.append(image_path)
+        if mismatched:
+            parent_index = image['parent']
+            album = albums[parent_index]
+            album_path = album['path']
+            album_image_list = None
+            value = [mismatch_reason, image_path]
+            if album_path not in album_result:
+                album_image_list = [value]
+                album_result[album_path] = album_image_list
+            else:
+                album_image_list = album_result[album_path]
+                album_image_list.append(value)
 
     util.pprint(album_result)
+
+def main():
+    with exiftool.ExifTool() as et:
+        main_with_exiftool(et)
 
 if __name__ == '__main__':
   main()
