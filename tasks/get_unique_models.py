b14048efd9b16950caf882614ed37efc3b987571
amitabh <buddhabacchan@gmail.com>
2021-02-04 09:32:10 -0800

    implementing get_unique_models.py

---
+++ tasks/get_unique_models.py
@@ -0,0 +1,15 @@
+import context; context.set_context()
+import os
+from pathlib import Path
+import logging
+import sys
+import json
+
+from datetime import datetime
+
+import exiftool
+
+import util
+import gphoto
+from gphoto.local_library import LocalLibrary
+from gphoto.imageutils import ImageUtils
