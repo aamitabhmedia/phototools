e9052793099301be7bd0a5a0da678bea4e687974
amitabh <buddhabacchan@gmail.com>
2021-02-19 18:26:45 -0800

    continue implementing download_google_album_images

---
+++ tasks/get_google_albums_summary.py
@@ -0,0 +1,87 @@
+import context; context.set_context()
+
+import os
+import json
+import logging
+
+import gphoto
+from gphoto.google_albums import GoogleAlbums
+from gphoto.google_images import GoogleImages
+from gphoto.google_album_images import GoogleAlbumImages
+
+def main():
+    gphoto.init()
+
+    GoogleAlbums.load_albums()
+    google_album_cache = GoogleAlbums.cache()
+    google_albums = google_album_cache['list']
+
+    GoogleImages.load_images()
+    google_image_cache = GoogleImages.cache()
+    google_images = google_image_cache['list']
+
+    GoogleAlbumImages.load_album_images()
+    google_album_image_cache = GoogleAlbumImages.cache()
+
+    result = []
+
+    # Loop through each google album
+    for google_album in google_albums:
+
+        google_album_id = google_album['id']
+
+        album_images = None
+        if google_album_id in google_album_image_cache:
+            album_images = google_album_image_cache[google_album_id]
+
+        count = 0
+        not_mine_count = 0
+        if album_images:
+            count = len(album_images)
+
+            for image_idx in album_images:
+
+                google_image = google_images[image_idx]
+                if 'mine' in google_image:
+                    mine = google_image['mine']
+                    if not mine:
+                        not_mine_count += 1
+
+        ownership = None
+        if not_mine_count == 0:
+            ownership = "All-Mine"
+        elif not_mine_count == count:
+            ownership = "None-Mine"
+        else:
+            ownership = f"{count - not_mine_count}" + " Partial"
+
+        title = None
+        if 'title' not in google_album:
+            title = "[EMPTY]" + google_album_id
+        else:
+            title = google_album['title']
+
+        album_result = {
+            'title': title,
+            'shared': google_album['shared'],
+            'image_count': count,
+            'ownership': ownership
+        }
+
+        result.append(album_result)
+
+    # Save to cache file also
+    cache_dir = gphoto.cache_dir()
+    file_path = os.path.join(cache_dir, "google_albums_summary.json")
+
+    try:
+        cache_file = open(file_path, "w")
+        json.dump(result, cache_file, indent=2)
+        cache_file.close()
+        logging.info(f"save_google_albums_summary: Successfully saved to '{file_path}'")
+        return True
+    except Exception as e:
+        logging.critical(f"save_google_albums_summary: unable to save google albums summary")
+
+if __name__ == '__main__':
+  main()
