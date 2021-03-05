4e88f958736057843cd29dad82e564d3f7e5af35
amitabh <buddhabacchan@gmail.com>
2021-03-04 21:26:03 -0800

    continue implementing single google library class

--- gphoto/goog_library.py
+++ gphoto/goog_library.py
@@ -36,13 +36,65 @@ class GoogLibrary:
         return GoogLibrary._cache
 
     # -----------------------------------------------------
+    # Cache an image
+    # -----------------------------------------------------
+    @staticmethod
+    def cache_image(mediaItem, google_image_ids, google_image_filenames):
+        mediaItemID = mediaItem['id']
+        google_image_ids[mediaItemID] = mediaItem
+
+        filename = mediaItem['filename']
+        google_image_filenames[filename] = mediaItemID
+
+    # -----------------------------------------------------
+    # cache images
+    # -----------------------------------------------------
+    @staticmethod
+    def cache_images():
+
+        # Initialize images fields of the cache
+        cache = GoogLibrary.cache()
+        google_images = []
+        google_image_ids = {}
+        google_image_filenames = {}
+        cache.images = google_images
+        cache.image_ids = google_image_ids
+        cache.image_filenames= google_image_filenames
+
+        service = GoogleService.service()
+        if not service:
+            logging.error("GoogleImages.cache_images: GoogleService.service() is not initialized")
+            return
+
+        # Get the first page of mediaItems
+        pageSize=25
+        response = service.mediaItems().list(
+            pageSize=25
+        ).execute()
+
+        mediaItems = response.get('mediaItems')
+        for mediaItem in mediaItems:
+            GoogleLibraryImages.cache_image(mediaItem, google_image_ids, google_image_filenames)
+        nextPageToken = response.get('nextPageToken')
+
+        # Loop through rest of the pages of mediaItems
+        while nextPageToken:
+            response = service.mediaItems().list(
+                pageSize=100,
+                pageToken=nextPageToken
+            ).execute()
+            for mediaItem in mediaItems:
+                GoogleLibraryImages.cache_image(mediaItem, google_image_ids, google_image_filenames)
+            nextPageToken = response.get('nextPageToken')
+
+    # -----------------------------------------------------
     # Cach Google Photos use API
     # -----------------------------------------------------
     @staticmethod
     def cache_library():
 
         GoogLibrary._cache = GoogleLibraryCache()
-        GoogleLibraryImages.cache_images()
+        GoogLibrary.cache_images()
 
     # --------------------------------------
     # Save media items to local file
