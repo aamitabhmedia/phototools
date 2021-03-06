4e85eabf4c322845845f5afe0f48cc1ac93751a3
amitabh <buddhabacchan@gmail.com>
2021-03-06 13:47:40 -0800

    continue implementing check_google_image_belongs_to_an_album

--- tasks/check_google_image_belongs_to_an_album.py
+++ tasks/check_google_image_belongs_to_an_album.py
@@ -6,6 +6,7 @@ import logging
 import util
 import gphoto
 from gphoto.local_library import LocalLibrary
+from gphoto.goog_library import GoogLibrary
 from gphoto.imageutils import ImageUtils
 
 # -----------------------------------------------------
@@ -22,9 +23,32 @@ def main():
         logging.error("Too few arguments.  See help")
         return
 
+    # Get arguments
     patterns = sys.argv[1:]
     print(patterns)
 
+    # Load cache
+    GoogLibrary.load_library()
+    cache = GoogLibrary.cache()
+    google_album_ids = cache['album_ids']
+    google_album_titles = cache['album_titles']
+    google_image_ids = cache['image_ids']
+    google_image_filenames = cache['image_filenames']
+    google_album_images = cache['album_images']
+    google_image_albums = cache['image_albums']
+
+    # Define the result
+      result = {
+          'album': {
+              'id': ....,
+              'title': ...
+          }
+      }
+
+    # Find images with the give patterns
+    for google_image_id, google_image in google_album_ids.items():
+        google_image_filename = google_image['filename']
+        if patterns in google_image_filename:
 
 if __name__ == '__main__':
   main()
