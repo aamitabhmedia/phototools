b4c732481a5cbb3987f233aae435541ce503b3fe
amitabh <buddhabacchan@gmail.com>
2021-03-08 22:16:18 -0800

    implementing local library metadata class

--- tasks/correct_spelline_local_library.py
+++ tasks/correct_spelline_local_library.py
@@ -7,8 +7,49 @@ import logging
 import util
 import gphoto
 from gphoto.local_library import LocalLibrary
-from gphoto.goog_library import GoogLibrary
+from gphoto.local_library_metadata import LocalLibraryMetadata
 from gphoto.imageutils import ImageUtils
+
+# -----------------------------------------------------
+# -----------------------------------------------------
+def main_library_type(old_word, new_word, library_type):
+
+    library_cache = LocalLibrary.cache_raw() if library_type == 'raw' else LocalLibrary.cache_jpg()
+    albums = library_cache.get('albums')
+    album_paths = library_cache.get('album_paths')
+    album_names = library_cache.get('album_names')
+    images = library_cache.get('images')
+    image_ids = library_cache.get('image_ids')
+
+    library_metadata_cache = LocalLibraryMetadata.cache_raw() if library_type == 'raw' else LocalLibraryMetadata.cache_jpg()
+
+    for album in albums:
+        album_path = album.get('path')
+        if old_word in album_path:
+            new_album_path = album_path.replace(old_word, new_word)
+            print(f"album='{album_path}'")
+            print(f"    old='{album_path}'")
+            print(f"    new='{new_album_path}'")
+
+    for image_path, image_metadata in library_metadata_cache.items():
+
+        desc = image_metadata.get('Description')
+        title = image_metadata.get('Title')
+        if desc is None:
+            desc = title
+        
+        if desc is None:
+            continue
+
+        # If misspelled word found in the images description
+        # then try to correct it
+        if old_word in desc:
+            old_desc = desc
+            new_desc = desc.replace(old_word, new_word)
+
+            print(f"image='{image_path}'")
+            print(f"    old='{old_desc}'")
+            print(f"    new='{new_desc}'")
 
 # -----------------------------------------------------
 # Main
@@ -21,12 +62,11 @@ def main():
     if len(sys.argv) < 3:
         logging("Too few args.  See help")
         return
-    word = sys.argv[1]
-    replacement = sys.argv[2]
+    old_word = sys.argv[1]
+    new_word = sys.argv[2]
 
-    LocalLibrary.load_library('jpg')()
-    local_cache = LocalLibrary.cache_jpg()
-    local_images = local_cache.get('images')
+    LocalLibrary.load_library()
+    main_library_type(old_word, new_word, 'raw')
 
 if __name__ == '__main__':
   main()
--- tests/test_dattimeoriginal_createdate_diff.py
+++ tests/test_dattimeoriginal_createdate_diff.py
@@ -15,20 +15,33 @@ def main():
     cache = LocalLibraryMetadata.cache_raw()
 
     for image_id, image in cache.items():
-        datetimeoriginal = image.get('DateTimeOriginal')
-        createdate = image.get('CreateDate')
+        desc = image.get('Description')
+        title = image.get('Title')
 
-        filename = os.path.basename(image_id)
-        if filename.startswith("PFILM"):
+        # filename = os.path.basename(image_id)
+        # if filename.startswith("PFILM"):
+        #     continue
+
+        # ext = image.get('FileTypeExtension')
+        # if ext and ext == 'mov':
+        #     continue
+
+        if '2009-10-31 Haloween party at Gambhir' in image_id:
             continue
 
-        ext = image.get('FileTypeExtension')
-        if ext and ext == 'mov':
+        if "2011-07-04 Amitabh's 50th Birthday Post Celeberation" in image_id:
             continue
 
-        if datetimeoriginal is None and createdate is not None:
+        if "2017 04 16 Vocal Performance M.A." in image_id:
+            continue
+
+        if "2009-08-06 Italy Trip" in image_id:
+            continue
+
+        if desc is not None and title is not None and desc != title:
             print(image_id)
-            print(f"    CreateDate='{createdate}'")
+            print(f"     desc='{desc}'")
+            print(f"    title='{title}'")
 
 
 if __name__ == '__main__':
