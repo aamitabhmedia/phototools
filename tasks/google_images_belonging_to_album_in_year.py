95d466bd7910606b901f3d9d09ff11f7da68ace5
amitabh <buddhabacchan@gmail.com>
2021-03-09 18:28:26 -0800

    implementing google image belonging to an album in a year

---
+++ tasks/google_images_belonging_to_album_in_year.py
@@ -0,0 +1,66 @@
+import context; context.set_context()
+
+import os
+import sys
+import logging
+
+import util
+import gphoto
+from gphoto.goog_library import GoogLibrary
+from gphoto.imageutils import ImageUtils
+
+
+# -----------------------------------------------------
+# Main
+# -----------------------------------------------------
+def main():
+    """
+    Specify the year in which to narrow the search of images belonging to an album
+    """
+    if len(sys.argv) < 2:
+        logging.error("Too few arguments.  See help")
+        return
+
+    # Get arguments
+    arg_year = sys.argv[1]
+
+    GoogLibrary.load_library()
+    google_cache = GoogLibrary.cache()
+    google_album_ids = google_cache['album_ids']
+    google_album_titles = google_cache['album_titles']
+    google_image_ids = google_cache['image_ids']
+    google_image_filenames = google_cache['image_filenames']
+    google_album_images = google_cache['album_images']
+    google_image_albums = google_cache['image_albums']
+
+    result_no_dateshot = []
+    result_albums = {}
+    result = {
+        'no-dateshot': result_no_dateshot,
+        'albums': result_albums
+    }
+
+    # Get dateshot.  If not there then report it
+    for google_image_id, google_image in google_image_ids.items():
+        dateShot = None
+        image_metadata = google_image.get('mediaMetadata')
+        if image_metadata:
+            dateShot = image_metadata.get('creationTime')
+        if dateShot is None:
+            result_no_dateshot.append({
+                'id': google_image_id,
+                'productUrl': google_image.get('productUrl')
+            })
+            continue
+
+    # We have a dateshot.  parse it and get the year
+    # If year does not math then ignore the image
+    image_year = dateShot.split('-')[0]
+    if arg_year != image_year:
+        continue
+
+
+
+
+if __name__ == '__main__':
+  main()
