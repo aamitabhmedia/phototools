7acf802a525ba0781054df49f1c2612f5af4f7f2
amitabh <buddhabacchan@gmail.com>
2021-05-11 21:20:22 -0700

    update

--- tasks/fix_pfilm_folder.py
+++ tasks/fix_pfilm_folder.py
@@ -22,7 +22,6 @@ def main():
         Given a root folder, for all its subfolders:
         1.  Remove "Sxxx - " prefix form sub folder
         2.  Set dateshot to 10AM for the date from the album name
-        3.  Add comments from the album name
     """
 
     # Parse arguments
@@ -48,9 +47,6 @@ def main():
         #       of images in that folder
         album_date = basedir[:10]
         fix_pfilm_dateshot(os.path.join(tgtdir, '*'), album_date, None, "8", False)
-
-        # 3. Add folder name as caption and rename files
-        ps_command = subprocess.run(["powershell", "-Command", f"-c -r '{tgtdir}'"], capture_output=True)
 
 if __name__ == '__main__':
   main()
