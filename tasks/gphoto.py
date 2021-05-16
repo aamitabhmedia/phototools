e6a960d05914407ed245521f764a953ee51145b9
amitabh <buddhabacchan@gmail.com>
2021-05-16 15:46:56 -0700

    updated

--- tasks/gphoto.py
+++ tasks/gphoto.py
@@ -3,25 +3,32 @@ import argparse
 import os
 import sys
 
+modules = [
+    {
+        'name': 'album',
+        'commands': ''
+    }
+]
+
 def parse_args():
     # Create the parser
     my_parser = argparse.ArgumentParser(prog='gphoto', description='Gphoto Command Line Interface')
 
     # Add the arguments
     # Add module
-    my_parser.add_argument('Module',
+    my_parser.add_argument('module',
                         metavar='module',
                         type=str,
                         help='Name of the module')
 
     # Add module commands
-    my_parser.add_argument('ModuleCommand',
-                        metavar='module_command',
+    my_parser.add_argument('command',
+                        metavar='command',
                         type=str,
                         help='Name of the module sub-command')
 
     # Add genera purpose switches
-    my_parser.add_argument('-v', action='count', help='make output more verbose')
+    my_parser.add_argument('-v', dest='verbose', action='count', help='make output more verbose')
 
     # Execute the parse_args() method
     args = my_parser.parse_args()
