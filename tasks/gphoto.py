f7eaff5343d57d5194c6fb317583d72a042f57f5
amitabh <buddhabacchan@gmail.com>
2021-05-16 16:46:01 -0700

    added defaults in UI for config

--- tasks/gphoto.py
+++ tasks/gphoto.py
@@ -1,48 +1,32 @@
-import argparse
+import fire
 
-import os
-import sys
 
-modules = [
-    {
-        'name': 'album',
-        'commands': ''
-    }
-]
+class AlbumModule(object):
 
-def parse_args():
-    # Create the parser
-    my_parser = argparse.ArgumentParser(prog='gphoto', description='Gphoto Command Line Interface')
+  def create(self, title, share:bool = True):
+    return f"creating album '{title}', as Shared={share}"
 
-    # Add the arguments
-    # Add module
-    my_parser.add_argument('module',
-                        metavar='module',
-                        type=str,
-                        help='Name of the module')
+  def get(self, title):
+    return f"return album '{title}'"
 
-    # Add module commands
-    my_parser.add_argument('command',
-                        metavar='command',
-                        type=str,
-                        help='Name of the module sub-command')
+class ImageModule(object):
 
-    # Add genera purpose switches
-    my_parser.add_argument('-v', dest='verbose', action='count', help='make output more verbose')
+  def upload(self, filename, album_id=None):
+    return f"Uploading file '{filename}', albu_id='{album_id}'"
 
-    # Execute the parse_args() method
-    args = my_parser.parse_args()
-    return args
+  def status(self):
+    return 'Image Status is GOOD'
 
-def gphoto_cli():
-    args = parse_args()
-    print(args)
+class Root(object):
 
-# -----------------------------------------------------
-# Main
-# -----------------------------------------------------
-def main():
-    gphoto_cli()
+  def __init__(self):
+    self.album = AlbumModule()
+    self.image = ImageModule()
+
+  def run(self):
+    # self.ingestion.run()
+    # self.digestion.run()
+    return 'Root complete'
 
 if __name__ == '__main__':
-  main()
+  fire.Fire(Root)
