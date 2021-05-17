2a2afcf1890389e4f8a1b50296f6cb715292d7a3
amitabh <buddhabacchan@gmail.com>
2021-05-16 17:47:21 -0700

    added defaults in UI for config

--- tasks/gphoto_album.py
+++ tasks/gphoto_album.py
@@ -3,7 +3,26 @@ import fire
 
 class GphotoAlbum(object):
 
-  def create(self, title, share:bool = True):
+  def create(self, folder, share:bool = True):
+    """
+    Parameters
+            ----------
+            folder : string
+                path to local album folder
+            share : bool
+                Make album shareable (default == sharable)
+
+            Returns
+            -------
+            string
+                Returns Google album id, if successful
+    """
+    # """
+    # Create an album and return album id
+    # Arguments:
+    #     title: Title of the album
+    #     --share | --noshare: make album shareable (--share is deafault)
+    # """
     return f"creating album '{title}', as Shared={share}"
 
   def get(self, title):
