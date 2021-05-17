704cdad6a59a3fd5bdff68a606f0ba1e418cc636
amitabh <buddhabacchan@gmail.com>
2021-05-17 08:40:23 -0700

    updated

--- tasks/gphoto_album.py
+++ tasks/gphoto_album.py
@@ -2,20 +2,21 @@ import context; context.set_context()
 import fire
 
 class GphotoAlbum(object):
+  """Module to handle Google album specific commands"""
 
   def create(self, folder, share:bool = True):
     """
     Parameters
-            ----------
-            folder : string
-                path to local album folder
-            share : bool
-                Make album shareable (default == sharable)
+    ----------
+    folder : string
+        path to local album folder
+    share : bool
+        Make album shareable (default == sharable)
 
-            Returns
-            -------
-            string
-                Returns Google album id, if successful
+    Returns
+    -------
+    string
+        Returns Google album id, if successful
     """
     # """
     # Create an album and return album id
@@ -23,7 +24,7 @@ class GphotoAlbum(object):
     #     title: Title of the album
     #     --share | --noshare: make album shareable (--share is deafault)
     # """
-    return f"creating album '{title}', as Shared={share}"
+    return f"creating album '{folder}', as Shared={share}"
 
   def get(self, title):
     return f"return album '{title}'"
