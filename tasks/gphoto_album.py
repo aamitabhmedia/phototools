33446e1464004925e10262cdd1694f0c343caded
amitabh <buddhabacchan@gmail.com>
2021-05-18 10:51:28 -0700

    updated

--- tasks/gphoto_album.py
+++ tasks/gphoto_album.py
@@ -6,9 +6,11 @@ class GphotoAlbum(object):
 
   def create(self, folder, share:bool = True):
     """Create album given 'local folder path', '--share' (default) to make it shareable"""
+
     return f"creating album '{folder}', as Shared={share}"
 
   def get(self, title=None, id=None):
     """Return 'album' object given the 'title' or 'id'"""
+    
     return f"return album for 'title={title}' or 'id={id}'"
 
